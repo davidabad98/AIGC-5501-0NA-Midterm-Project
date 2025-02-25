2d8511dceb319a4633f8f69eb2360a45a3631557
Rizvan Nanif <joyrizvan@gmail.com>
2025-02-24 19:46:51 -0500

    User input added

--- src/bot.py
+++ src/bot.py
@@ -4,20 +4,27 @@ from classification import *
 import warnings
 warnings.filterwarnings("ignore")
 # Example usage
+import time
 
 def run_bot():
-    user_question =  "When was beyonce born?"
-    cm = classificationModel()
-    predicted_cluster = cm.classify_question(user_question)
-    
-    df = load_file(cm.folder_path, cm.filename, 'csv')
-    best_question, best_answer, match_score = find_best_match(user_question, df, predicted_cluster)
+    while True:
+        user_question =  input("User Question: ")
+        if user_question == 'qt':
+            break
+        else: 
+            start = time.time()
+            cm = classificationModel()
+            predicted_cluster = cm.classify_question(user_question)
+            
+            df = load_file(cm.folder_path, cm.filename, 'csv')
+            best_question, best_answer, match_score = find_best_match(user_question, df, predicted_cluster)
+            end = time.time()
 
-    # Print result
-    print(f"User Question: {user_question}")
-    print(f"Best Matched Question: {best_question}")
-    print(f"Match Score: {match_score}%")
-    print(f"Answer: {best_answer}")
-
+        # Print result
+        #print(f"User Question: {user_question}")
+        print(f"Best Matched Question: {best_question}")
+        print(f"Match Score: {match_score}%")
+        print(f"Answer: {best_answer}")
+        print('It took', end-start, 'seconds.')
 if __name__ == '__main__':
     run_bot()
