11f8df1d022fadaf54e19f162d0be8d9ca0c9beb
Rizvan Nanif <joyrizvan@gmail.com>
2025-02-24 20:38:34 -0500

    nltk files added download.py

--- src/download.py
+++ src/download.py
@@ -5,3 +5,5 @@ nltk.download("wordnet")  # Conceptual relationship between words
 nltk.download('punkt')  # Pretrained tokenizer for English
 nltk.download('averaged_perceptron_tagger')  # Pretrained English POS Tagger
 nltk.download('omw-1.4')  # Open multilingual wordnet for multi-language support
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
